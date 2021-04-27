531de7f3ea3efb55c417b1b8eebd98a598c8d501
Sara Decker <sdecker@west-inc.com>
2021-04-27 14:05:44 -0600

    change service methods to use declarative approach so that components using the service just need to reference the observable property instead of calling a service method; no need for ngOnInit anymore

--- angular-rxjs/src/app/products/product-list.component.ts
+++ angular-rxjs/src/app/products/product-list.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { EMPTY, Observable, of } from 'rxjs';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
@@ -9,24 +8,20 @@ import { ProductService } from './product.service';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products$ = this.productService.products$.pipe(
+    catchError((err) => {
+      this.errorMessage = err; // todo since we changed the ChangeDetectionStrategy, need to push this change
+      // return of([]); // this is one option; or use EMPTY
+      return EMPTY;
+    })
+  );;
 
   constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts().pipe(
-      catchError((err) => {
-        this.errorMessage = err; // todo since we changed the ChangeDetectionStrategy, need to push this change
-        // return of([]); // this is one option; or use EMPTY
-        return EMPTY;
-      })
-    );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
