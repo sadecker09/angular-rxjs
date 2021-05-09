cfac739bb86822a679691ecd836c492abb812d8b
Sara Decker <sdecker@west-inc.com>
2021-05-09 15:59:26 -0600

    filter the stream...
    filter the products list by using a filter operator; for now, hardcode the selected filtered; will refactor in next steps

--- angular-rxjs/src/app/products/product-list.component.ts
+++ angular-rxjs/src/app/products/product-list.component.ts
@@ -1,17 +1,18 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { EMPTY } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$.pipe(
     catchError((err) => {
@@ -19,15 +20,35 @@ export class ProductListComponent {
       // return of([]); // this is one option; or use EMPTY
       return EMPTY;
     })
-  );;
+  );
 
-  constructor(private productService: ProductService) {}
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$.pipe(
+    map((products) =>
+      products.filter((product) =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
+  );
+
+  constructor(
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService
+  ) {}
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+   this.selectedCategoryId = +categoryId;
   }
 }
